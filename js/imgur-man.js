44b84bdf96113c63fe4050fe571c1d7c8dfc2162
Richard Towers <richard_towers@Richard-Towers-Macbook.local>
2013-02-27 23:03:56 +0000

    Adding fabric and bootstrap. Drawing some dots.

--- js/imgur-man.js
+++ js/imgur-man.js
@@ -1,1 +1,18 @@
-window.alert('Hello World!');
+(function () {
+    'use strict';
+    var canvas = new fabric.StaticCanvas('canvas', {
+        backgroundColor: '#000'
+    });
+    var width = canvas.getWidth();
+    var height = canvas.getHeight();
+    for(var i = 0; i < width; i += 20) {
+        var dot = new fabric.Circle({
+            top: 20,
+            left: i,
+            radius: 5,
+            fill: '#ff0'
+        });
+        canvas.add(dot);
+    }
+    canvas.renderAll();
+})();
--- ts/imgur-man.ts
+++ ts/imgur-man.ts
@@ -1,4 +1,25 @@
+///<reference path="fabricjs.d.ts"/>
+
 (function () {
 	'use strict';
-	window.alert('Hello World!');
+
+	// Initialize canvas:
+	// create a wrapper around native canvas element (with id="canvas")
+	var canvas = new fabric.StaticCanvas('canvas', { backgroundColor: '#000' });
+
+	var width = canvas.getWidth();
+	var height = canvas.getHeight();
+
+	for(var i = 0; i < width; i += 20) {
+		var dot = new fabric.Circle({
+			top: 20,
+			left: i,
+			radius: 5,
+			fill: '#ff0'
+		});
+		canvas.add(dot);
+	}
+
+	canvas.renderAll();
+
 })();
